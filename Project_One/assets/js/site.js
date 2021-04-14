2efb601a5c1d371e5c96586491b13209c5f9698f
Chinmeri <cnwagwu@hawk.iit.edu>
2021-04-13 20:23:48 -0500

    Add java elements

--- Project_One/assets/js/site.js
+++ Project_One/assets/js/site.js
@@ -3,3 +3,17 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+
+function ValidationEvent() {
+   var a = document.forms["form-one"]["first_name"].value;
+   var b = document.forms["form-one"]["last_name"].value;
+   var c = document.forms["form-one"]["email"].value;
+   var d = document.forms["form-one"]["phone-num"].value;
+   var e = document.forms["form-one"]["dob"].value;
+   if ((a == "") || (b == "") || (c == "") || (d == "") || (e == "")){
+     alert("Please enter appropriate Info in all the spaces provied.");
+   }
+   else{
+     alert("Thank you.Your information has been received");
+   }
+ }
