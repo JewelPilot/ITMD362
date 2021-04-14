b4f991fadfc8b64af4302347b98b981ee8cae744
Chinmeri <cnwagwu@hawk.iit.edu>
2021-04-13 20:51:46 -0500

    Add Proper id

--- Project_One/assets/js/site.js
+++ Project_One/assets/js/site.js
@@ -1,15 +1,15 @@
-// jQuery 3.x-style ready event and locally scoped $
+
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
 
 function ValidationEvent() {
-   var a = document.forms["form-one"]["first_name"].value;
-   var b = document.forms["form-one"]["last_name"].value;
-   var c = document.forms["form-one"]["email"].value;
-   var d = document.forms["form-one"]["phone-num"].value;
-   var e = document.forms["form-one"]["dob"].value;
+   var a = document.forms["fone"]["first_name"].value;
+   var b = document.forms["fone"]["last_name"].value;
+   var c = document.forms["fone"]["email"].value;
+   var d = document.forms["fone"]["phone-num"].value;
+   var e = document.forms["fone"]["dob"].value;
    if ((a == "") || (b == "") || (c == "") || (d == "") || (e == "")){
      alert("Please enter appropriate Info in all the spaces provied.");
    }
